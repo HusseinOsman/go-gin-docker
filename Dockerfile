d4aba1c8fc1d71cfa017f7eaa9b2cca83797c666
husseinosman <hussein.osman.fg@gmail.com>
2018-10-01 15:40:40 +0200

    add gorm and make a migration

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ WORKDIR /app
 
 ADD . /app
 RUN go get github.com/gin-gonic/gin
+RUN go get -u github.com/jinzhu/gorm
+
 RUN go build ./main.go
 
 EXPOSE 80
--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/go-sql-driver/mysql"
+	"fmt"
+	"time"
 )
 
 var DB = make(map[string]string)
+
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
@@ -58,7 +62,31 @@ func setupRouter() *gin.Engine {
 	return r
 }
 
+type Product struct {
+  Model
+  Code string
+  Price uint
+  Title string
+}
+
+// Base Model's definition
+type Model struct {
+  ID        uint `gorm:"primary_key"`
+  CreatedAt time.Time  `gorm:"column:createdAt"` 
+  DeletedAt *time.Time  `gorm:"column:deletedAt"` 
+}
+
 func main() {
+	db, err := gorm.Open("mysql", "user:pass@tcp(172.17.0.3)/go_gin?charset=utf8&parseTime=True&loc=Local")
+	defer db.Close()
+	if err != nil {
+		fmt.Println("db err := ",err)
+	}
+	db.AutoMigrate(&Product{})
+
+	// Add index for columns `title` with given name `idx_title`
+	db.Model(&Product{}).AddIndex("idx_title", "title")
+
 	r := setupRouter()
 	r.Run(":8082")
 }
